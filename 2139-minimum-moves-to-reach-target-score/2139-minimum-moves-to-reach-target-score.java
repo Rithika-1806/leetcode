5b381fff07011ca68638a2cf82a9256eb721cc50
Rithika Krishnan <23cs086@drngpit.ac.in>
2026-06-19 17:28:21 +0530

    Time: 0 ms (100%), Space: 42 MB (91.43%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        int count=0;
+        while(target>1 && maxDoubles>0){
+            if(target%2==0){
+                target=target/2;
+                maxDoubles--;
+            }
+            else{
+                target-=1;
+            }
+            count++;
+        }
+
+        if(target>1){
+            count=count+(target-1);
+        }
+        return count;
+    }
+}
